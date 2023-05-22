2a6d8e92c43cfba1269e0367a031e2546357c399
Jéssica <jessicanathany.web@gmail.com>
2023-05-22 08:13:53 -0300

    correcting BaseRepository

--- src/the-office.infrastructure/Data/Repositories/BaseRepository.cs
+++ src/the-office.infrastructure/Data/Repositories/BaseRepository.cs
@@ -5,24 +5,6 @@ namespace the_office.infrastructure.Data.Repositories
 {
     public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : Entity, new()
     {
-        public Task<IEnumerable<TEntity>> GetAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<TEntity> GetByCode(Guid code)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<TEntity> GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task IBaseRepository<TEntity>.SaveChanges()
-        {
-            throw new NotImplementedException();
-        }
+       
     }
 }
