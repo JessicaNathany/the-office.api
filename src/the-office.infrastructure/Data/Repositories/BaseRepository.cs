961a9df7cfbb01323413fb4157474c150201321b
Jéssica <jessicanathany.web@gmail.com>
2023-05-22 08:14:05 -0300

    correcting basde repository

--- src/the-office.infrastructure/Data/Repositories/BaseRepository.cs
+++ src/the-office.infrastructure/Data/Repositories/BaseRepository.cs
@@ -5,6 +5,24 @@ namespace the_office.infrastructure.Data.Repositories
 {
     public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : Entity, new()
     {
-       
+        public Task<IEnumerable<TEntity>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByCode(Guid code)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
