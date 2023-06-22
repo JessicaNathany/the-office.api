57c8aff309b39e7d04b2c6f33e4122b7b54e7714
Jéssica <jessicanathany.web@gmail.com>
2023-06-22 08:53:32 -0300

    created RemoveCharacterHandler and creating UpdadeCharacterHandler

---
+++ src/the-office.api.application/Characters/Handlers/UpdateCharacterHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using the_office.api.application.Characters.Messaging.Requests;
+using the_office.api.application.Characters.Messaging.Response;
+using the_office.api.application.Common.Commands;
+using the_office.domain.Repositories;
+using the_office.domain.Shared;
+
+namespace the_office.api.application.Characters.Handlers
+{
+    public class UpdateCharacterHandler : ICommandHandler<UpdateCharacterRequest, CharacterResponse>
+    {
+        private readonly ICharacterRepository _characterRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public UpdateCharacterHandler(ICharacterRepository characterRepository, IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _characterRepository = characterRepository;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public Task<Result<CharacterResponse>> Handle(UpdateCharacterRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+
+            // to be continued
+        }
+    }
+}
