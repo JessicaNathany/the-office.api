c14c70875f575490cc8fcda561a1e7cdcccd7bc2
Regis <regisbarros21@gmail.com>
2023-06-24 20:43:22 -0300

    Add health check for database

--- src/the-office.api/Infrastructure/HealthChecks/HealthChecks.cs
+++ src/the-office.api/Infrastructure/HealthChecks/HealthChecks.cs
@@ -1,34 +1,27 @@
-using System.Net.Mime;
-using System.Text.Json;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
+using the_office.infrastructure.Data.Context;
 
 namespace the_office.api.Infrastructure.HealthChecks;
 
 public static class HealthChecks
 {
+    public static IServiceCollection AddHealthChecksConfiguration(this IServiceCollection services)
+    {
+        if (services == null) throw new ArgumentNullException(nameof(services));
+
+        services.AddHealthChecks()
+            .AddDbContextCheck<TheOfficeDbContext>(); 
+
+        return services;
+    }
+    
     public static IApplicationBuilder UseHealthChecks(this IApplicationBuilder app)
     {
         app.UseHealthChecks("/health",
             new HealthCheckOptions()
             {
-                ResponseWriter = async (context, report) =>
-                {
-                    var result = new
-                    {
-                        status = report.Status.ToString(),
-                        errors = report.Entries.Select(e => new
-                        {
-                            key = e.Key,
-                            value = Enum.GetName(typeof(HealthStatus), e.Value.Status)
-                        })
-                    };
-
-                    var jsonResponse = JsonSerializer.Serialize(result);
-
-                    context.Response.ContentType = MediaTypeNames.Application.Json;
-                    await context.Response.WriteAsync(jsonResponse);
-                }
+               ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
             });
 
         return app;
